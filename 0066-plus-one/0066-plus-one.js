c22b593a4fc86ad3cb376e5b975b2006ebb382bd
Shaikh Mohammed Shahrukh <60780126+Skabir34@users.noreply.github.com>
2026-07-17 23:03:06 +0530

    Time: 1 ms (8.64%), Space: 53.3 MB (77.79%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    return (BigInt(digits.join("")) + 1n)
+        .toString()
+        .split("")
+        .map(Number);
+};
